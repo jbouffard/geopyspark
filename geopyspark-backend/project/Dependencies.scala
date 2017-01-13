334da744b611f510345ff5f5c84881d0f1caae3d
jbouffard <jbouffard@azavea.com>
2017-01-13 11:07:16 -0500

    Changed GeoTrellis version in Dependencies
    Signed-off-by: jbouffard <jbouffard@azavea.com>

--- geopyspark-backend/project/Dependencies.scala
+++ geopyspark-backend/project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val geotrellis = "org.locationtech.geotrellis" %% "geotrellis-spark" % "1.0.4-SNAPSHOT"
+  val geotrellis = "org.locationtech.geotrellis" %% "geotrellis-spark" % Version.geotrellis
 
   val sparkCore = "org.apache.spark"            %% "spark-core"       % "1.2.2"
 }
