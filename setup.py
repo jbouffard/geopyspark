4ca75901cf0e14d0e06b748a37dfc052ddfa2540
jbouffard <jbouffard@azavea.com>
2017-04-26 16:34:21 -0400

    Revertted travis to how it was before

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ JAR_PATH = 'geopyspark/jars/'
 class Installer(install):
     def run(self):
         if not os.path.isfile(JAR_PATH):
-            subprocess.call(['wget', URL + JAR, '-P', JAR_PATH])
-            #subprocess.call(['curl', '-L', URL + JAR, '-o', JAR_PATH + JAR])
+            subprocess.call(['curl', '-L', URL + JAR, '-o', JAR_PATH + JAR])
         install.run(self)
 
 
