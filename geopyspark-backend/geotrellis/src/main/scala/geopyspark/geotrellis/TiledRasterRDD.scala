7e6f42c411b2b44541f3b4755f5c0cbbc8b9206a
jbouffard <jbouffard@azavea.com>
2017-06-06 10:33:38 -0400

    Added protobuf codecs to the protobufs package
    Added ProtoBufCodec
    
    Updated ProtoBufCodecs
    
    Updated proto files
    
    Updated ProtoBufCodec
    
    Updated tileProtoBuf codecs
    
    Added ProtoBufUnionCodec
    
    point commit on protobuf tile
    
    Removed ProtoBufUnionCodec
    
    Added Proto to the begininng of the ProtoBuf classes
    
    Finished TileProtoBuf
    
    Added TupleProtoBuf
    
    Updated Implicits
    
    Cleaned up ProtoBufCodec
    
    Cleaned up the ProtoBufCodecs
    
    ProtoBufCodec now extends Serializable
    
    Added package.scala to geopyspark.geotrellis
    
    Updated Scala code to use ProtoBufCodecs
    
    Updated the Python ProtoBuf classes
    
    Made MultibandTileProtoBuf its own trait

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/TiledRasterRDD.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/TiledRasterRDD.scala
@@ -1,6 +1,10 @@
 package geopyspark.geotrellis
 
+import geopyspark.geotrellis._
 import geopyspark.geotrellis.GeoTrellisUtils._
+
+import protos.tileMessages._
+import protos.tupleMessages._
 
 import geotrellis.proj4._
 import geotrellis.raster._
@@ -61,6 +65,8 @@ abstract class TiledRasterRDD[K: SpatialComponent: AvroRecordCodec: JsonFormat: 
 
   /** Encode RDD as Avro bytes and return it with avro schema used */
   def toAvroRDD(): (JavaRDD[Array[Byte]], String) = PythonTranslator.toPython(rdd)
+
+  def toProtoRDD(): JavaRDD[Array[Byte]]
 
   def layerMetadata: String = rdd.metadata.toJson.prettyPrint
 
@@ -304,9 +310,11 @@ class SpatialTiledRasterRDD(
   def lookup(
     col: Int,
     row: Int
-  ): (java.util.ArrayList[Array[Byte]], String) = {
+  //): (java.util.ArrayList[Array[Byte]], String) = {
+  ): java.util.ArrayList[Array[Byte]] = {
     val tiles = rdd.lookup(SpatialKey(col, row))
-    PythonTranslator.toPython(tiles)
+    //PythonTranslator.toPython(tiles)
+    PythonTranslator.toPython[MultibandTile, ProtoMultibandTile](tiles)
   }
 
   def reproject(
@@ -469,6 +477,9 @@ class SpatialTiledRasterRDD(
 
   def toDouble(converted: RDD[(SpatialKey, MultibandTile)]): TiledRasterRDD[SpatialKey] =
     SpatialTiledRasterRDD(zoomLevel, MultibandTileLayerRDD(converted, rdd.metadata))
+
+  def toProtoRDD(): JavaRDD[Array[Byte]] =
+    PythonTranslator.toPython[(SpatialKey, MultibandTile), ProtoTuple](rdd)
 }
 
 
@@ -637,6 +648,9 @@ class TemporalTiledRasterRDD(
 
   def toDouble(converted: RDD[(SpaceTimeKey, MultibandTile)]): TiledRasterRDD[SpaceTimeKey] =
     TemporalTiledRasterRDD(zoomLevel, MultibandTileLayerRDD(converted, rdd.metadata))
+
+  def toProtoRDD(): JavaRDD[Array[Byte]] =
+    PythonTranslator.toPython[(SpaceTimeKey, MultibandTile), ProtoTuple](rdd)
 }
 
 
@@ -648,6 +662,17 @@ object SpatialTiledRasterRDD {
   ): SpatialTiledRasterRDD = {
     val md = metadata.parseJson.convertTo[TileLayerMetadata[SpatialKey]]
     val tileLayer = MultibandTileLayerRDD(PythonTranslator.fromPython[(SpatialKey, MultibandTile)](javaRDD, Some(schema)), md)
+
+    SpatialTiledRasterRDD(None, tileLayer)
+  }
+
+  def fromProtoEncodedRDD(
+    javaRDD: JavaRDD[Array[Byte]],
+    metadata: String
+  ): SpatialTiledRasterRDD = {
+    val md = metadata.parseJson.convertTo[TileLayerMetadata[SpatialKey]]
+    val tileLayer = MultibandTileLayerRDD(
+      PythonTranslator.fromPython[(SpatialKey, MultibandTile), ProtoTuple](javaRDD, ProtoTuple.parseFrom), md)
 
     SpatialTiledRasterRDD(None, tileLayer)
   }
@@ -740,6 +765,17 @@ object TemporalTiledRasterRDD {
   ): TemporalTiledRasterRDD = {
     val md = metadata.parseJson.convertTo[TileLayerMetadata[SpaceTimeKey]]
     val tileLayer = MultibandTileLayerRDD(PythonTranslator.fromPython[(SpaceTimeKey, MultibandTile)](javaRDD, Some(schema)), md)
+
+    TemporalTiledRasterRDD(None, tileLayer)
+  }
+
+  def fromProtoEncodedRDD(
+    javaRDD: JavaRDD[Array[Byte]],
+    metadata: String
+  ): TemporalTiledRasterRDD = {
+    val md = metadata.parseJson.convertTo[TileLayerMetadata[SpaceTimeKey]]
+    val tileLayer = MultibandTileLayerRDD(
+      PythonTranslator.fromPython[(SpaceTimeKey, MultibandTile), ProtoTuple](javaRDD, ProtoTuple.parseFrom), md)
 
     TemporalTiledRasterRDD(None, tileLayer)
   }
