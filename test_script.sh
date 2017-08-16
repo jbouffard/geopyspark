348c07b2a7a84a8c65a1c6c4e43c7a95e765037b
jbouffard <jbouffard@azavea.com>
2017-08-16 15:19:59 -0400

    The output of the nbconversions will not be saved

--- test_script.sh
+++ test_script.sh
@@ -7,7 +7,7 @@ testpattern="*_test.py"
 
 for f in $notebookpath/*.ipynb
 do
-  jupyter nbconvert --to notebook --ExecutePreprocessor.startup_timeout=1200 --ExecutePreprocessor.iopub_timeout=1200 --ExecutePreprocessor.timeout=-1 --execute $f
+  jupyter nbconvert --to notebook --ExecutePreprocessor.startup_timeout=1200 --ExecutePreprocessor.iopub_timeout=1200 --ExecutePreprocessor.timeout=-1 --execute --stdout $f > /dev/null
 done
 
 for f in $testpath$testpattern $testpath/**/$testpattern;
