a3c467b546cdec51bfb8984dd411d8fed6ded02d
jbouffard <jbouffard@azavea.com>
2017-10-18 15:30:00 -0400

    Made all TileLayer classes serializable

--- geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/TileLayer.scala
+++ geopyspark-backend/geotrellis/src/main/scala/geopyspark/geotrellis/TileLayer.scala
@@ -27,7 +27,7 @@ import scala.reflect.{ClassTag, classTag}
 import scala.collection.JavaConverters._
 import scala.util.Try
 
-abstract class TileLayer[K: ClassTag] {
+abstract class TileLayer[K: ClassTag] extends Serializable {
   def rdd: RDD[(K, MultibandTile)]
   def keyClass: Class[_] = classTag[K].runtimeClass
   def keyClassName: String = keyClass.getName
