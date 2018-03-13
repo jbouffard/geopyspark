620c87b9ec03f460425052fd661c0154a50c982f
Jacob Bouffard <jbouffard@azavea.com>
2018-03-13 12:01:25 -0400

    Travis will upload the egg and whl files of GPS

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e
 set -x
 
 aws s3 cp geopyspark/jars/geotrellis-backend-assembly-*.jar s3://geopyspark-dependency-jars/ \
+  && python3 setup.py bdist_egg bdist_wheel \
+  && aws s3 cp dist/geopyspark*.egg s3://geopyspark-dependency-jars/ \
+  && aws s3 cp dist/geopyspark*.whl s3://geopyspark-dependency-jars/ \
   && cd geopyspark-backend \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project geotrellis-backend" publish \
   && ./sbt -Dbintray.user=$BINTRAY_USER -Dbintray.pass=$BINTRAY_PASS "project vectorpipe" publish \
