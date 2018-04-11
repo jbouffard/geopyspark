f13178cbf2de47b345378a2ad2aa78ffe22f9bdf
Austin Heyne <aheyne@ccri.com>
2018-04-11 18:44:14 -0400

    WORKING!
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geopyspark/geomesa/__init__.py
+++ geopyspark/geomesa/__init__.py
@@ -9,6 +9,8 @@ ensure_pyspark()
 
 from geopyspark import get_spark_context, create_python_rdd
 from py4j.java_gateway import java_import
+from pyspark.sql.types import UserDefinedType, StructField, BinaryType
+from pyspark.sql import Row
 
 
 class GeoMesaSpark(object):
@@ -22,6 +24,7 @@ class GeoMesaSpark(object):
         java_import(self.jvm, "org.locationtech.geomesa.spark.api.java.JavaGeoMesaSpark")
         java_import(self.jvm, "org.locationtech.geomesa.spark.api.java.JavaSpatialRDDProvider")
         java_import(self.jvm, "org.locationtech.geomesa.spark.api.java.JavaSpatialRDD")
+        java_import(self.jvm, "org.locationtech.geomesa.spark.jts.util.JavaGeometryUDT")
 
     def apply(self, params):
         provider = self.jvm.JavaGeoMesaSpark.apply(params)
@@ -60,4 +63,26 @@ class SpatialRDDProvider(object):
         return create_python_rdd(self.jvm.SerDe.javaToPython(jrdd), self.sc)
 
 
-__all__ = ['GeoMesaSpark', 'SpatialRDDProvider']
+class GeometryUDT(UserDefinedType):
+    @classmethod
+    def sqlType(self):
+        return StructField("wkb", BinaryType(), False)
+
+    @classmethod
+    def module(cls):
+        return 'geopyspark'
+
+    @classmethod
+    def scalaUDT(cls):
+        return 'org.apache.spark.sql.jts.GeometryUDT'
+
+    def serialize(self, obj):
+        if (obj is None): return None
+        return Row(obj.toBytes)
+
+    def deserialize(self, datum):
+        jvm = get_spark_context()._gateway.jvm
+        return jvm.JavaGeometryUDT.deserialize(datum[0])
+
+
+__all__ = ['GeoMesaSpark', 'SpatialRDDProvider', 'GeometryUDT']
